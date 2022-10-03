dd2885bfd9b6511cf0cb4a92350bf621a5763e03
Almazova Kristina <cristinaalmaz1702@gmail.com>
2022-10-03 15:52:02 +0300

    Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число:");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+void Degree(int num, int agr)
+{
+     int i = 1;
+     int numC=0;
+     while (i<=agr)
+     {
+      numC=num*num;
+       i++;
+     }
+     Console.WriteLine($"{numC}");
+}
+Degree(numA,numB);
