f58298f9afbdc487976f660748a6be0b2f5d8e63
Almazova Kristina <cristinaalmaz1702@gmail.com>
2022-10-03 16:04:42 +0300

    Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число:");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+
+void SumOfDigits(int num)
+{
+     int i = 1;
+     int remainder=0;
+     int sum = 0;
+     while (num>0)
+     {
+      remainder = num % 10;
+      num = num/10;
+      sum = sum + remainder;
+       i++;
+     }
+     Console.WriteLine($"{sum}");
+}
+SumOfDigits(numA);
